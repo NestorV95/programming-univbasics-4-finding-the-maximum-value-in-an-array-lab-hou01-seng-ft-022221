fe8cb28c3958ef6819cf8962d7e50ce41e177f36
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-04 17:42:18 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,8 @@
-def find_max_value(array)
+def find_max_value(array, index)
   count = 0 
   
   while count < array.length do 
-    if array[count] > array
+    if array[count] > index
       return count
     end 
     count +=
